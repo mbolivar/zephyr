c0bc0711eef856985d85352bad21cd85478efa00
Martí Bolívar <marti.bolivar@nordicsemi.no>
2020-03-04 16:41:20 -0800

    [HACK] boards: nrf52_pca10040: demonstrate out of tree runners
    Just for demo purposes.
    
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>

--- boards/arm/nrf52_pca10040/board.cmake
+++ boards/arm/nrf52_pca10040/board.cmake
@@ -1,5 +1,48 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(PY_FILE "/tmp/out_of_tree.py")
+
+file(WRITE "${PY_FILE}" "\
+import pathlib
+import os
+import sys
+
+sys.path.append(str(pathlib.Path(os.getenv('ZEPHYR_BASE')) /
+                    'scripts' /
+                    'west_commands'))
+
+from runners.core import ZephyrBinaryRunner, RunnerCaps
+
+class OutOfTreeRunner(ZephyrBinaryRunner):
+    '''Runner front-end for nrfjprog.'''
+
+    def __init__(self, cfg, foo):
+        super().__init__(cfg)
+        self.foo = foo
+
+    @classmethod
+    def name(cls):
+        return 'oot'
+
+    @classmethod
+    def capabilities(cls):
+        return RunnerCaps(commands={'flash'})
+
+    @classmethod
+    def do_add_parser(cls, parser):
+        parser.add_argument('--foo', required=True)
+
+    @classmethod
+    def create(cls, cfg, args):
+        return OutOfTreeRunner(cfg, args.foo)
+
+    def do_run(self, command, **kwargs):
+        self.logger.info(f'hello there, {self.foo}')
+")
+
+
+board_add_oot_runner(oot "${PY_FILE}" "--foo=\"from board.cmake\"")
+
 board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
 board_runner_args(pyocd "--target=nrf52" "--frequency=4000000")
